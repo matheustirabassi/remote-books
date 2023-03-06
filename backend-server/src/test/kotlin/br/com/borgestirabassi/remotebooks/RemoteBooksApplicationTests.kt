abdba8d1ed64972b20882cdde31323281be1f5da
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2023-03-06 20:58:08 -0300

    Adiciona backend Spring Boot ao projeto
    Implementa #1.
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

---
+++ backend-server/src/test/kotlin/br/com/borgestirabassi/remotebooks/RemoteBooksApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.borgestirabassi.remotebooks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemoteBooksApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
