930331aecbf0d1bb9e555e4b87a321d3d99ab8f5
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2023-03-07 13:38:21 -0300

    Cria entidade e tabela Category
    - Adiciona script para criar arquivos .sql de migração
    
    Implementa #4.
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

---
+++ backend-server/src/main/kotlin/br/com/borgestirabassi/remotebooks/domain/Category.kt
@@ -0,0 +1,27 @@
+package br.com.borgestirabassi.remotebooks.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+/**
+ * Representa uma categoria de um livro.
+ *
+ * @property id O identificador da categoria.
+ * @property name O nome da categoria
+ */
+@Entity
+@Table(name = "CATEGORY")
+data class Category(
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+
+    @Column(name = "NAME")
+    val name: String,
+)
---
+++ backend-server/src/main/resources/scripts/create-db-migration-file.sh
@@ -0,0 +1,7 @@
+current_time=$(date "+%Y%m%d%H%M%S")
+description=`echo $@ | tr '[:upper:]' '[:lower:]'`
+description=`echo $description | sed -e "s/ /\_/g"`
+file_name="V"$current_time"__"$description".sql"
+
+touch "../db/migration/"$file_name
+echo "Arquivo criado: $file_name"
