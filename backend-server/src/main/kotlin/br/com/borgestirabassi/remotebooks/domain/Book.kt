ba52e2e9aa02fee73f01e03e0dc68a5e777fa5c5
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2023-04-29 10:02:32 -0300

    Adiciona entidade que representa um livro
    - Adiciona script V20230429095722__cria-tabela-livro.sql
    para criar a tabela BOOK.
    
    Implementa #7.
    
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

---
+++ backend-server/src/main/kotlin/br/com/borgestirabassi/remotebooks/domain/Book.kt
@@ -0,0 +1,43 @@
+package br.com.borgestirabassi.remotebooks.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.persistence.Temporal
+import jakarta.persistence.TemporalType
+import java.util.Date
+
+/**
+ * Representa um livro.
+ *
+ * @property id O identificador.
+ * @property title O título.
+ * @property sinopse O resumo.
+ * @property imageLink O link que representa a imagem do livro.
+ * @property registrationDate A data do registro do livro no sistema, representado por um timestamp.
+ */
+@Entity
+@Table(name = "BOOK")
+data class Book(
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "TITLE")
+    val title: String,
+
+    @Column(name = "SINOPSE", length = 3000)
+    val sinopse: String,
+
+    @Column(name = "IMAGE_LINK", length = 5000)
+    val imageLink: String,
+
+    @Column(name = "REGISTRATION_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    val registrationDate: Date,
+)
