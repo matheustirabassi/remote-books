9c3907cc375fd1aa7b2818a9811ccf79bc78f5e1
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2023-06-17 13:55:41 -0300

    Corrige fuso horário para o brasileiro
    Signed-off-by: Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>

--- backend-server/src/main/kotlin/br/com/borgestirabassi/remotebooks/utils/extensions/StringExtensions.kt
+++ backend-server/src/main/kotlin/br/com/borgestirabassi/remotebooks/utils/extensions/StringExtensions.kt
@@ -3,12 +3,19 @@ package br.com.borgestirabassi.remotebooks.utils.extensions
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.TimeZone
 
-val yearFormatter = SimpleDateFormat("yyyy-MM-dd")
+val yearFormatter = setDefaultTimeZone(SimpleDateFormat("yyyy-MM-dd"))
 private const val YEAR_FORMAT_LENGTH = 10
 
-val standardDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:SS")
+val standardDateFormat = setDefaultTimeZone(SimpleDateFormat("yyyy-MM-dd HH:mm:SS"))
 private const val STANDARD_DATE_FORMAT_LENGTH = 19
+
+private fun setDefaultTimeZone(simpleDateFormat: SimpleDateFormat): SimpleDateFormat {
+    simpleDateFormat.timeZone = TimeZone.getTimeZone("America/Sao_Paulo")
+
+    return simpleDateFormat
+}
 
 /**
  * Converte uma String para Date
